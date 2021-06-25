d0078deafadfbdfb399fc5e9cde5b5c076b2e340
johnGachihi <johngachihi3@gmail.com>
2021-06-25 18:38:50 +0300

    Add tests for RequestHandlerImpl

--- src/main/kotlin/router/RequestHandlerImpl.kt
+++ src/main/kotlin/router/RequestHandlerImpl.kt
@@ -3,12 +3,16 @@ package router
 import server.RequestHandler
 import core.Request
 import core.Response
+import intercepting.InterceptorManager
 
 class RequestHandlerImpl(
-    private val endpointFactory: EndpointFactory
+    private val endpointFactory: EndpointFactory,
+    private val interceptorManager: InterceptorManager
 ) : RequestHandler {
     override fun handleRequest(request: Request): Response {
-        return endpointFactory.getEndpoint(request.actionCode)
-            .handleRequest(request)
+        val req = interceptorManager.interceptRequest(request)
+        val endpoint = endpointFactory.getEndpoint(request.actionCode)
+        val res = endpoint.handleRequest(req)
+        return interceptorManager.interceptResponse(res)
     }
 }
---
+++ src/test/kotlin/router/RequestHandlerImplTest.kt
@@ -0,0 +1,120 @@
+package router
+
+import core.Request
+import core.Response
+import createMockRequest
+import intercepting.InterceptorManager
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(MockKExtension::class)
+internal class RequestHandlerImplTest {
+    @RelaxedMockK
+    lateinit var interceptorManager: InterceptorManager
+
+    @RelaxedMockK
+    lateinit var endpointFactory: EndpointFactory
+
+    @InjectMockKs
+    lateinit var requestHandler: RequestHandlerImpl
+
+    @Test
+    fun `handleRequest calls InterceptorManager#interceptRequest once`() {
+        requestHandler.handleRequest(createMockRequest())
+
+        verify(exactly = 1) {
+            interceptorManager.interceptRequest(any())
+        }
+    }
+
+    @Test
+    fun `handleRequest calls InterceptorManager#interceptRequest with the Request argument it receives`() {
+        val request = mockk<Request>(relaxed = true)
+        requestHandler.handleRequest(request)
+
+        verify {
+            interceptorManager.interceptRequest(request)
+        }
+    }
+
+    @Test
+    fun `handleRequest calls EndpointFactory#getEndpoint once`() {
+        requestHandler.handleRequest(createMockRequest())
+
+        verify(exactly = 1) {
+            endpointFactory.getEndpoint(any())
+        }
+    }
+
+    @Test
+    fun `handleRequest calls EndpointFactory#getEndpoint with the action-code from the request it received`() {
+        val expectedActionCode = 123
+
+        requestHandler.handleRequest(
+            createMockRequest(withActionCode = expectedActionCode))
+
+        verify {
+            endpointFactory.getEndpoint(expectedActionCode)
+        }
+    }
+
+    @Test
+    fun `handleRequest calls Endpoint returned by EndpointFactory#getEndpoint once`() {
+        val endpoint = mockk<Endpoint>(relaxed = true)
+        every { endpointFactory.getEndpoint(any()) } returns endpoint
+
+        requestHandler.handleRequest(createMockRequest())
+
+        verify(exactly = 1) { endpoint.handleRequest(any()) }
+    }
+
+    @Test
+    fun `handleRequest calls Endpoint#handleRequest with result of InterceptorManager#interceptRequest`() {
+        val endpoint = mockk<Endpoint>(relaxed = true)
+        every { endpointFactory.getEndpoint(any()) } returns endpoint
+        val request = createMockRequest()
+        every { interceptorManager.interceptRequest(any()) } returns request
+
+        requestHandler.handleRequest(createMockRequest())
+
+        verify { endpoint.handleRequest(request) }
+    }
+
+    @Test
+    fun `handleRequest calls InterceptorManager#interceptResponse once`() {
+        requestHandler.handleRequest(createMockRequest())
+
+        verify(exactly = 1) {
+            interceptorManager.interceptResponse(any())
+        }
+    }
+
+    @Test
+    fun `handleRequest calls InterceptorManager#interceptResponse, with result of Endpoint#handleRequest`() {
+        val endpoint = mockk<Endpoint>()
+        val response = mockk<Response>()
+        every { endpoint.handleRequest(any()) } returns response
+        every { endpointFactory.getEndpoint(any()) } returns endpoint
+
+        requestHandler.handleRequest(mockk(relaxed = true))
+
+        verify { interceptorManager.interceptResponse(response) }
+    }
+
+    @Test
+    fun `handleRequest returns result of InterceptorManager#interceptResponse`() {
+        val expectedResponse = mockk<Response>()
+        every { interceptorManager.interceptResponse(any()) } returns expectedResponse
+
+        val actualResponse = requestHandler.handleRequest(mockk(relaxed = true))
+
+        assertEquals(expectedResponse, actualResponse)
+    }
+}
