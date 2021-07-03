c6a96eb7a41250a350bd9ffd7b15f97d5fa14da0
johnGachihi <johngachihi3@gmail.com>
2021-07-03 20:51:59 +0300

    Update DI container
    Also replace EndpointFactory implementation from EndpointFactoryImpl
    to KoinEndpointFactory
    Todo: Separate appModules into system and user-app modules

--- src/main/kotlin/di/modules.kt
+++ src/main/kotlin/di/modules.kt
@@ -1,10 +1,32 @@
 package di
 
+import app.actionMap1
+import app.endpoints.EntryEndpoint
+import app.endpoints.RfidEndpoint
+import app.repos.VisitRepository
+import app.repos.VisitRepositoryImpl
+import app.services.EntryService
+import app.services.EntryServiceImpl
+import db.HibernateSessionContextManager
 import db.HibernateSessionContextManagerImpl
 import org.hibernate.Session
 import org.koin.dsl.module
+import router.*
+import server.RequestHandler
 
 
 val appModules = module {
+    // System modules
     factory<Session> { HibernateSessionContextManagerImpl.getCurrentSession() }
+    single<RequestHandler> { RequestHandlerImpl(get(), get(), get()) }
+    single<EndpointFactory> { KoinEndpointFactory(actionMap1) }
+    single<HibernateSessionContextManager> { HibernateSessionContextManagerImpl }
+    single<ExceptionHandler> { ExceptionHandlerImpl() }
+
+    // App modules
+    single { EntryEndpoint(get()) }
+    single<EntryService> { EntryServiceImpl(get()) }
+    single<VisitRepository> { VisitRepositoryImpl(get()) }
+
+    single { RfidEndpoint() }
 }
