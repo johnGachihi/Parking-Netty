a55a34385d92e84e9c3e0fb02934071bcfa9ff19
johnGachihi <johngachihi3@gmail.com>
2021-07-03 20:50:36 +0300

    ...

---
+++ src/test/kotlin/app/endpoints/MockServer.kt
@@ -0,0 +1,33 @@
+package app.endpoints
+
+import com.digitalpetri.modbus.codec.ModbusTcpPayload
+import com.digitalpetri.modbus.requests.WriteMultipleRegistersRequest
+import core.Request
+import core.Response
+import core.modbus.ModbusWriteRequest
+import io.netty.buffer.ByteBuf
+import server.RequestHandler
+import kotlin.math.ceil
+
+class MockServer(
+    private val requestHandler: RequestHandler,
+) {
+    fun sendModbusWriteRequest(address: Int, data: ByteBuf): Response {
+        val request = formModbusWriteRequest(address, data)
+        return handleRequest(request)
+    }
+
+    private fun formModbusWriteRequest(address: Int, data: ByteBuf): ModbusWriteRequest {
+        val pdu = WriteMultipleRegistersRequest(
+            address,
+            ceil(data.readableBytes() / 2.0).toInt(),
+            data
+        )
+        val modbusTcpPayload = ModbusTcpPayload(1, 1, pdu)
+        return ModbusWriteRequest(address, modbusTcpPayload)
+    }
+
+    private fun handleRequest(request: Request): Response {
+        return requestHandler.handleRequest(request)
+    }
+}
