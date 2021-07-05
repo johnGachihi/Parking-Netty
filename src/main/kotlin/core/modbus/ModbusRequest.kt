a312b19ae6eca10def71507fd0406d78bf0cef48
johnGachihi <johngachihi3@gmail.com>
2021-07-05 16:07:39 +0300

    Add ModbusRequest interface which all modbus request will be derived.

--- src/main/kotlin/core/modbus/ModbusRequest.kt
+++ src/main/kotlin/core/modbus/ModbusRequest.kt
@@ -5,9 +5,11 @@ import com.digitalpetri.modbus.requests.WriteMultipleRegistersRequest
 import io.netty.buffer.ByteBuf
 import core.Request
 
+interface ModbusRequest : Request
+
 class ModbusWriteRequest(
     override val actionCode: Int,
     val modbusTcpPayload: ModbusTcpPayload
-) : Request {
+) : ModbusRequest {
     val data: ByteBuf = (modbusTcpPayload.modbusPdu as WriteMultipleRegistersRequest).values
 }
